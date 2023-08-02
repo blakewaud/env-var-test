dac3aec2a61c601591dbd3793b5294cab5386e43
Blake Waud <blake@robocorp.com>
2023-08-02 15:05:32 -0400

    Fix creation of `env_vars` dict

--- tasks.py
+++ tasks.py
@@ -9,5 +9,5 @@ log.add_sensitive_variable_name("SECRET")
 @task
 def expose_env_vars():
     """Logs all environment variables available in the context."""
-    env_vars = os.environ
+    env_vars = {key: value for key, value in os.environ.items()}
     log.info(f"Environment variables: {json.dumps(env_vars, indent=4, sort_keys=True)}")
