15a8d6c095c5c03d5c5692dcbfe170e7b437d339
Blake Waud <blake@robocorp.com>
2023-08-02 15:00:39 -0400

    Sort output and try to redact secrets

--- tasks.py
+++ tasks.py
@@ -1,10 +1,13 @@
 import os
+import json
 from robocorp.tasks import task
 from robocorp import log
+
+log.add_sensitive_variable_name("SECRET")
 
 
 @task
 def expose_env_vars():
     """Logs all environment variables available in the context."""
     env_vars = os.environ
-    log.info(f"Environment variables: {env_vars!r}")
+    log.info(f"Environment variables: {json.dumps(env_vars, indent=4, sort_keys=True)}")
